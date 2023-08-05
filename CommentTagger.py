9f04138145f223c149bb9d4c9e109d1b410f8c7a
Raghavpy227 <123891968+Raghavpy227@users.noreply.github.com>
2023-08-05 20:59:13 +0100

    Add files via upload

--- CommentTagger.py
+++ CommentTagger.py
@@ -49,19 +49,15 @@ df= pd.read_csv(os.path.join(path,"RS_2021-07.csv"))
 #df.drop(["Unnamed: 0"],axis=1,inplace=True)
 
 regex_dict= {
-        "racial_slur": [r"(?:nigga|nigger|coon|uncle tom|negro|niggerhead|house slave|monkeyboy)"],
+        "racial_slur": [r"(?:nigga|nigger|uncle tom|negro|niggerhead|house slave|monkeyboy)"],
         "self_harm": [r"(?:kill yourself|commit suicide)"],
-        "Homophobia":[r"(?:lesbo|faggot|cocksucker|fag)"],
-        "Incivility":[r"(?:dickhead|twat|cunt|whore|retard|bitch|asshole|dimwit|bullshit)"],
-        
-        "harrassment":[r"(?:penis|pussy|tits|dick)"],
-        "Xenophobia":[r"(?:paki|wops|japs|chinese|go back to your country)"]
-        
-        
-        
+        "Homophobia":[r"(?:lesbo|faggot|fag)"],
+        "Incivility":[r"(?:dickhead|twat|cunt|whore|fuck you|fuck u|retard|bitch|asshole|dimwit|bullshit|cocksucker|motherfuck)"],
+        "harrassment":[r"(?:penis|pussy|tits|dick|sexy bitch|suck dick)"]
     }
 #"personal_id":[r"(?:[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})"]
 #"Spam": [r"(?:\(\S+\) \S+: .*(\S)(?: *\1){9,}.*)"
+is_violation=[]
 violation=[]
 violation_sentence=[]
 full_context=df["context"]
@@ -71,32 +67,55 @@ subreddit=[]
 new_created=[]
 counter_row=0
 df.drop(["context"],axis=1,inplace=True)
+
 new_context=[]
 for i in full_context:
     new_context.append(i.split("',"))
 counter=0 
+better_split=[]
+diff_sp='",'
 
+for i in new_context:
+    inter=[]
+    for j in i:
         
-    
-df["context"]  = new_context
-for i in df["context"]:
+     if diff_sp in j:
+        #print("Hi")
+        inter.append(re.split(diff_sp,j))
+    else:
+        inter.append(j)
+    better_split.append(inter)    
+        
+        
+count=0    
+df["context"]  = better_split
+
+for i in better_split:
     counter_context=0
     for j in i :
-        check=0
-        for k in regex_dict.keys(): 
-            pattern = re.compile('|'.join(regex_dict[k]), re.IGNORECASE)
-            matches = pattern.findall(j)
-            if len(matches)>0:
-                violation.append(k)
-                violation_sentence.append(j)
-                subreddit.append(df["subreddit"][counter_row])
-                new_created.append(df["created_date"][counter_row])
-                new_id.append(df["id"][counter_row])
-                subcontexts.append(i[0:counter_context])
-                check=1
-            elif len(matches)==0:
-                continue
+        for m in j: 
+            mini_context=0
+            check=0 
+            for k in regex_dict.keys(): 
+                pattern = re.compile('|'.join(regex_dict[k]), re.IGNORECASE)
+                matches = pattern.findall(m)
+                if len(matches)>0: 
+                    violation.append(k)
+                    violation_sentence.append(m)
+                    subreddit.append(df["subreddit"][counter_row])
+                    new_created.append(df["created_date"][counter_row])
+                    new_id.append(df["id"][counter_row])
+                    t=[]
+                    t.append(i[0:counter_context])
+                    t.append(j[0:mini_context])
+                    subcontexts.append(t)
+                    check=1
+                    is_violation.append("Yes")
+                elif len(matches)==0:
+                    continue
+                mini_context+=1
         counter_context+=1     
+        
             
     if check == 0: 
         violation.append("No Violations")
@@ -105,6 +124,7 @@ for i in df["context"]:
         new_created.append(df["created_date"][counter_row])
         new_id.append(df["id"][counter_row])
         subcontexts.append(j)
+        is_violation.append("No")
                      
                     
             
@@ -116,10 +136,12 @@ df["id"]=new_id
 df["subreddit"]=subreddit
 df["created_date"]=new_created
 df["context"]=subcontexts
+df["is_violation"]=is_violation
 df["violation"]=violation
 df["violation sentence"]=violation_sentence   
 
-df.to_csv(os.path.join(path,"RS_2021-07_violations.csv"),index=False)          
+
+df.to_csv(os.path.join(path,"RS_2012-03_violations.csv"),index=False)          
     
 
             
