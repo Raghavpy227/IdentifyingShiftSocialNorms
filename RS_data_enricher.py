48c3f064054bed87b143fcbd26780a0bb6a098c5
Raghavpy227 <123891968+Raghavpy227@users.noreply.github.com>
2023-08-23 22:51:05 +0100

    Add files via upload

--- RS_data_enricher.py
+++ RS_data_enricher.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul 15 14:34:39 2023
-
-@author: sragh
-"""
-#importing all the necessary libraries
-
 import pandas as pd
 import os
 import praw
@@ -13,21 +5,73 @@ from praw.models import Comment, Submission
 from praw.models.comment_forest import CommentForest
 import datetime
 from multiprocessing import Pool
+import boto3
 
-path = r"C:\Users\sragh\OneDrive\Documents\Dissertation\Data\submissions"
-all_csv=os.listdir(path)
-dataframes=dict()
-username="" #Enter Your Reddit Username
-password="" #Enter your Reddit password
-secret="" Enter your Reddit secret key from reddit
-client_id="" #Enter Client ID from Reddit
+ 
 
+ 
+
+path = r"C:\Users\sragh\OneDrive\Documents\Dissertation\Data\s3_submissions_downloads"
+s3_client = boto3.Session(aws_access_key_id='AKIA6IIIHUJETGQMT54O',aws_secret_access_key='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')
+s3=s3_client.resource('s3')
+s3_bucket=s3.Bucket('csv-extracted-files')
+s3_bucket_processed=s3.Bucket('comment-extracted-reddit-files')
+
+ 
+
+ 
+
+all_objects=s3_bucket.objects.all()
+all_csv=[]
+
+ 
+
+ 
+
+for i in all_objects:
+    all_csv.append(i.key)
+
+ 
+
+already_processed_objects=s3_bucket_processed.objects.all()
+processed_csv=[]
+
+ 
+
+ 
+
+for i in already_processed_objects: 
+    processed_csv.append(i.key)    
+
+ 
+
+ 
+
+to_process_csv=[]
 for i in all_csv:
-    if i[-1]=='v':
-        df=pd.read_csv(os.path.join(path,i))
-        df=df[df["num_comments"]>=20]
-        if len(df["id"]) >0: 
-            dataframes[i]=df
+    if i not in processed_csv:
+        to_process_csv.append(i)
+
+ 
+
+ 
+
+username="theredditapienjoyer"
+password="Thakurji@2002"
+secret="pkvNaeebVm700GggCqouig_cZuvt6Q"
+client_id="4lUm-oVC66Bh1z3veqM9Bw"
+
+ 
+
+ 
+
+s3_client = boto3.client('s3',aws_access_key_id='AKIA6IIIHUJEZKQH5NEZ',aws_secret_access_key='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')
+
+ 
+
+ 
+
+ 
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -44,8 +88,16 @@ def convertToDate(df):
     df["created_date"] = created_date   
     return df
 
+ 
+
+ 
+
 def getSubmission(id):
     return Submission(reddit,id)
+
+ 
+
+ 
 
 def getCommentTree(df):
     context=[]
@@ -56,34 +108,56 @@ def getCommentTree(df):
             submission = Submission(reddit,i)
             tree= submission.comments._comments
             all_comments=[]
-          
+
+ 
+
             for i in tree:
                 comment= i.body
                 all_comments.append(comment)
-         
+
+ 
+
             context.append(all_comments) 
             count=count+1
-            print(count) 
-    
+            print(count/len(df["id"]))
+
+ 
+
         except:  
             print("except")
             context.append([])
             count=count+1
-            
-        
-    df["context"]=context
+
+ 
+
+ 
+
+    df["context"]=context    
     return df
 count=0
 
-for k,v in dataframes.items():
-    if "context" in v.columns:
-        count=count+1
-        print("File number "+str(count))
+ 
+
+ 
+num_of_comments=25
+for i in to_process_csv: 
+    if i[9:10]!='6':
         continue
-    df=convertToDate(v)
+    print(i)
+    s3_client.download_file('csv-extracted-files',Key=i,Filename=os.path.join(path,i))
+    df=pd.read_csv(os.path.join(path,i))
+    if "context" in df.columns:
+        os.remove(os.path.join(path,i))
+        continue
+    while len(df["num_comments"]) >= 20000: 
+        df=df[df["num_comments"]>= num_of_comments]
+        num_of_comments+=25
+    print("number of sumbissions" +str(len(df["num_comments"])))
+    df=convertToDate(df)
     df=getCommentTree(df)
-    df.to_csv(os.path.join(path,k))
     count=count+1
-    print(count)
-    
-    
+    print("File Number "+str(count)+" completed")
+    df.to_csv(os.path.join(path,i),index=False)
+    s3_client.upload_file(os.path.join(path,i),'comment-extracted-reddit-files',i)
+    os.remove(os.path.join(path,i))
+    print("File Number "+str(count)+" uploaded")
