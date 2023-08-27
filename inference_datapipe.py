b561fa655e5ea0b37b66eb812537502f60bdcafe
Raghavpy227 <123891968+Raghavpy227@users.noreply.github.com>
2023-08-27 11:37:30 +0100

    Update inference_datapipe.py

--- inference_datapipe.py
+++ inference_datapipe.py
@@ -1,6 +1,6 @@
 '''
 The Aim of this code is to generate the pytorch checkpoints of 20000 entries from Inference data for both the eras(2005-2010, 2011-2015).Each of them would have corresponding date
-for InputExample objects. This code is specifically for 2005-2010 data frame . Change the dataframe 2011-2015. look at datapipe_2.py to do so
+for InputExample objects. This code is specifically for 2005-2010 data frame . Change the dataframe 2011-2015. look at data_pipeline2.py to do so
 '''
 
 import pandas as pd
