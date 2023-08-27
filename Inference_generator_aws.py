890669fe4438d05c3d102ee6e20b545f447f1524
Raghavpy227 <123891968+Raghavpy227@users.noreply.github.com>
2023-08-27 10:01:57 +0100

    Update Inference_generator_aws.py

--- Inference_generator_aws.py
+++ Inference_generator_aws.py
@@ -3,9 +3,12 @@
 Created on Thu Aug 17 11:28:13 2023
 
 @author: py22715
+The aim of this code is to download data loader from AWS bucket and the pre-trained OpenPrompt Models(2005,2011) 
+
+Generate Inference and then upload it to AWS bucket as a Pytorch checkpoint. 
 """
 
-
+#Importing all the necessary libraries
 import pandas as pd
 import os
 from multiprocessing import Pool
@@ -13,11 +16,13 @@ import boto3
 import torch
 from tqdm.contrib import tenumerate
 
+#loading the pretrained models
 path = r"C:\Users\py22715\OneDrive - University of Bristol\Documents\Python Scripts"
 chkpt_2005=torch.load(os.path.join(path,"PromptModel2005.pt"))
 chkpt_2011=torch.load(os.path.join(path,"PromptModel2011.pt"))
 promptModel2005=chkpt_2005["model"]
 promptModel2011=chkpt_2011["model"]
+#Setting up a client session
 s3_client = boto3.Session(aws_access_key_id='AKIA6IIIHUJETGQMT54O',aws_secret_access_key='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')
 s3=s3_client.resource('s3')
 s3_bucket=s3.Bucket('comment-extracted-reddit-files')
@@ -25,27 +30,30 @@ s3_bucket=s3.Bucket('comment-extracted-reddit-files')
 all_objects=s3_bucket.objects.all()
 all_csv=[]
 
+# Getting all name of all objects present in the bucket
 for i in all_objects:
     all_csv.append(i.key)
 
 all_pt_2005=[] 
 all_pt_2011=[]  
 all_processed=[]
+#Creating a list for dataloaders for which Inference is already generated.
 for i in all_csv:
     if i[len(i)-4:len(i)-3] == "e":
         all_processed.append(i[12:21])
         
        
-  
+#creates a list that needs to be processed by different OpenPromptModels, If the user aims to rewrite the already made inference then comment the first If block.   
 for i in all_csv:
     if i[12:21] in all_processed:
         continue
     if i[len(i)-2:len(i)] == "pt" and i[12:16]=="2005":
         all_pt_2005.append(i)
     if i[len(i)-2:len(i)] == "pt" and i[12:16]=="2011":
-        all_pt_2011.append(i)    
+        all_pt_2011.append(i)   
+#Creating a S3_Client        
 s3_client = boto3.client('s3',aws_access_key_id='AKIA6IIIHUJEZKQH5NEZ',aws_secret_access_key='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')
-
+#This function downloads the checkpoint files and generates the inference and uploads them
 def inference_generator(promptModel,files):
     for i in files:
         print("Generating Inference for "+i)
